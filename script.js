84d97b8f483cace0523c6cc020b0f893f9dba3a6
belanghay <113161874+belanghay@users.noreply.github.com>
2024-11-05 11:47:54 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+window.onload = function() {
+    alert("hi myangsss");
+    alert("Life is hard but I know you can do it");
+    alert("I'M always here for you");
+    alert("There will always be someone who is proud of you as long as I live");
+};
