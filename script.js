efe22afe08e4e9b7be967ee036def2cce3b17ea1
belanghay <113161874+belanghay@users.noreply.github.com>
2025-02-09 15:30:32 -1200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 window.onload = function() {
-    alert("hi myangsss");
+    alert("HI MINGS");
     alert("Life is hard but I know you can do it");
     alert("I'M always here for you");
     alert("There will always be someone who is proud of you as long as I live");
